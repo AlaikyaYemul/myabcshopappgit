786cca33e6bc1cbafd052381b867a8cff2f5206d
AlaikyaYemul <alaikyayemul5@gmail.com>
2024-12-29 17:14:06 +0530

    DateTime Exploring done.

--- abcshopapp/src/com/abcshopapp/main/LocalDateDemo.java
+++ abcshopapp/src/com/abcshopapp/main/LocalDateDemo.java
@@ -8,21 +8,45 @@ public class LocalDateDemo {
 	public static void main(String[] args) {
 
 		LocalDate dob = LocalDate.of(2001, 3, 13);
-		
+
 		LocalDate today = LocalDate.now();
-		
+
 		System.out.println("Today's date : "+today);
-		
+
 		System.out.println("DOB : "+dob);
-		
+
 		String s = "2022-10-03";
 		Date date = Date.valueOf(s);
 		LocalDate date1 = date.toLocalDate();
-		
+
 		System.out.println("Before Conversion : "+date);
 		System.out.println("Date : "+date1);
-		
 
+		//date manipulation
+		LocalDate afterDays = today.plusDays(35);
+
+		System.out.println("\nAfter 35 days, date is : "+afterDays);
+		
+		System.out.println("\nAfter 4 years : "+today.plusYears(4));
+		
+		System.out.println("\nAfter 25 weeks : "+today.plusWeeks(25));
+		
+		System.out.println("\nAfter 9 months : "+today.plusMonths(9));
+		
+		System.out.println("\nDay of month : "+today.getDayOfMonth());
+		
+		System.out.println("\nDay of year : "+today.getDayOfYear());
+		
+		System.out.println("\nValue of Month : "+today.getMonthValue());
+		
+		System.out.println("\nYear : "+today.getYear());
+		
+		System.out.println("\nDay of week : "+today.getDayOfWeek());
+		
+		System.out.println("\nLength of month : "+today.lengthOfMonth());
+		
+		System.out.println("\nLength of year : "+today.lengthOfYear());
+		
 	}
 
 }
